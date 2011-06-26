2a85eccf875965b4da98e0d29e4dda0b49bab42a
Chris Van Vranken <cessationoftime@gmail.com>
2011-06-26 17:08:19 -0400

    Add to comment for RobolectricRunAllTests 	modified:   src/test/java/com/seventheye/robolectric/sqlite/RunAllRobolectricTestsWithSQLiteTestSuite.java

--- src/test/java/com/seventheye/robolectric/sqlite/RunAllRobolectricTestsWithSQLiteTestSuite.java
+++ src/test/java/com/seventheye/robolectric/sqlite/RunAllRobolectricTestsWithSQLiteTestSuite.java
@@ -10,7 +10,12 @@ import com.seventheye.robolectric.sqlite.util.PackagedSuite.PackagedClasses;
  * Make sure to ONLY run this as a TEST SUITE.
  * If you work with Eclipse and run this as a test suite,
  * and then choose an individual test from the list you will
- * end up running the test with the H2 database instead of SQLite
+ * end up running the test with the H2 database instead of SQLite.
+ * 
+ * robolectric git repository and robolectric-sqlite git repository
+ * may need to be located in the same parent folder for this test
+ * to succeed otherwise it will not be able to find src/test/resources
+ * of robolectric. 
  */
 @RunWith(PackagedSuite.class)
 @PackagedClasses("com.xtremelabs.robolectric")
